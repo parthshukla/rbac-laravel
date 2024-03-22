c58a4d2291f975bee3932e3cfe1a89d8cc0ef17e
Parth Shukla <shuklaparth@hotmail.com>
2024-03-22 14:12:34 +0530

    [TASK]Folder update after composer dump autoload

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -23,30 +23,11 @@ class ComposerAutoloaderInitc1c641f08a8a0f79d27f3c78a62462fc
         }
 
         spl_autoload_register(array('ComposerAutoloaderInitc1c641f08a8a0f79d27f3c78a62462fc', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInitc1c641f08a8a0f79d27f3c78a62462fc', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
-        if ($useStaticLoader) {
-            require __DIR__ . '/autoload_static.php';
-
-            call_user_func(\Composer\Autoload\ComposerStaticInitc1c641f08a8a0f79d27f3c78a62462fc::getInitializer($loader));
-        } else {
-            $map = require __DIR__ . '/autoload_namespaces.php';
-            foreach ($map as $namespace => $path) {
-                $loader->set($namespace, $path);
-            }
-
-            $map = require __DIR__ . '/autoload_psr4.php';
-            foreach ($map as $namespace => $path) {
-                $loader->setPsr4($namespace, $path);
-            }
-
-            $classMap = require __DIR__ . '/autoload_classmap.php';
-            if ($classMap) {
-                $loader->addClassMap($classMap);
-            }
-        }
+        require __DIR__ . '/autoload_static.php';
+        call_user_func(\Composer\Autoload\ComposerStaticInitc1c641f08a8a0f79d27f3c78a62462fc::getInitializer($loader));
 
         $loader->register(true);
 
